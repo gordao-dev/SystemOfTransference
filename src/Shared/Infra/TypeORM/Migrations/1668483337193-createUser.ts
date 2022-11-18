1ab4ca0856a9bf8e248de4e788a164beec083eb3
gordao-dev <contato.gmds@gmail.com>
2022-11-18 08:43:06 -0300

    feat(users): created a migration

---
+++ src/Shared/Infra/TypeORM/Migrations/1668483337193-createUser.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class createUser1668483337193 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
